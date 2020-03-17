b8332c80277574c4a2f9a383dcd804e36eb70e80
pseudoprosciutto <55765602+pseudoprosciutto@users.noreply.github.com>
2020-03-17 16:37:31 -0400

    Create Player.java
    Player object is created at start of program. This class will represent the players character. Player will have an inventory and keep track of the room they are in. Player will have a weight limit of items it can pick up. Player may eventually have other stats or objects which it will need to record  and keep track of.

---
+++ Player.java
@@ -0,0 +1,75 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+/**
+ * Player class is created at game launch and acts as the player's avatar.
+ * The player will have an inventory which will hold items. 
+ * tells item it is being held
+ * if holding item can use
+ * the items will have a limit until weaing back pack
+ * The player will be able to check which room it is in. 
+ * 
+ * @author Matthew Sheehan  
+ * @version 3/17/2020
+ */
+public class Player
+{
+    // instance variables - replace the example below with your own
+    private int itemLimit;
+    private int itemsHeld;
+    private int currentRoom;
+    private HashSet<Items> Inventory;
+    private boolean haveBackpack;
+
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player()
+    {
+        currentRoom = 1; //Starting Room Number 1
+        itemsHeld = 0; //Start with no items being held
+        itemLimit = 2; // can only hold 2 items until a backpack
+        haveBackpack = false; //no backpack at stat
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod(int y)
+    {
+
+    }
+        /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod2(int y)
+    {
+
+    }
+        /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod3(int y)
+    {
+
+    }
+        /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod4(int y)
+    {
+    
+    }
+}
