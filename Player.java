f14226906f5fc824ef90b77b5bf9fff59d042b9d
pseudoprosciutto <55765602+pseudoprosciutto@users.noreply.github.com>
2020-03-18 12:54:41 -0400

    Update Player.java
    added @param room for constructor to place player in a room when initialized.
    
    currentRoom field now has getter and setter to keep track of player positioning. These will be accessed when player changes room
    
    Item functionality updated:
    void takeItems(item)  - removes item from room adds to inventory
    boolean haveItem(item) - checks to see if item is contained in Inventory
    void dropItem(item) - adds item to room and removes from Player Inventory
    getItemString() - lists out inventory

--- Player.java
+++ Player.java
@@ -23,15 +23,37 @@ public class Player
 
     /**
      * Constructor for objects of class Player
+     * @param room  starting location of room object for player
      */
-    public Player()
+    public Player(Room room)
     {
-       // currentRoom = 1; //Starting Room Number 1
+       Inventory = new HashSet<Items>();
+       currentRoom = room; //Starting Room Number 1
         itemsHeld = 0; //Start with no items being held
         itemLimit = 1; // can only hold 2 items until a backpack
-        haveBackpack = false; //no backpack at stat
+        haveBackpack = false; //no backpack at start
     }
-    //Backpack Functionality:
+    
+    //Room functionality: ***********************
+    /**
+     * current room player is in
+     * @return current room
+     */
+    public Room getCurrentRoom()
+    {
+        return currentRoom;
+    }
+    
+    /**
+     * set current room player is in
+     */
+    public void setCurrentRoom(Room room)
+    {
+        currentRoom = room;
+    }
+    
+    
+    //Backpack Functionality: ****************************
     /**
      * Reflect player picking up backpack
      * haveBackpack = true
@@ -41,8 +63,9 @@ public class Player
         itemLimit = 3;
         haveBackpack = true;
     }   
+    
     /**
-     * Reflect player dropping up backpack
+     * Reflect player dropping backpack
      * haveBackpack = true
      */
     public void dropBackpack()
@@ -50,6 +73,7 @@ public class Player
         itemLimit = 1;
         haveBackpack = false;
     }
+    
     /**
      * is Player wearing backpack?
      * @return haveBackPack 
@@ -60,27 +84,60 @@ public class Player
     }
     
     
-    
+    //Item Functionality: ****************
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Return a string describing the player's items, for example
+     * "Items you are holding: lamp, rock".
+     * @return Lists room's items or "You are not holding any items".
      */
-    public void sampleMethod(int y)
+    private String getItemsString()
     {
-
+        String returnString ="";
+        if(Inventory.isEmpty()){
+            returnString = "You are not holding any items";
+        } else {
+            returnString = "Items you are holding:";
+            String temp = "";
+            for(Items item : Inventory) {
+                temp += ", " + item.getName() ; 
+            }
+            returnString += temp.substring(1);
+        }
+        return returnString;
     }
     
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Check to see if Item is on player
+     * @param  item     an Items object
      */
-    public void sampleMethod2(int y)
+    public boolean haveItem(Items item)
     {
-
+        boolean haveItem = Inventory.contains(item);
+        return haveItem;
+    }
+    
+    /**
+     * take item and place into Inventory
+     * @param  item     an Items object
+     */
+    public void takeItem(Items item)
+    {
+        if(!haveItem(item) || currentRoom.haveItem(item)){
+            Inventory.add(item);
+            currentRoom.removeItem(item);
+        }
+    }
+    
+    /**
+     * Drop item and remove from Inventory. 
+     *
+     * @param  item     an Items object
+     */
+    public void dropItem(Items item)
+    {
+        if(haveItem(item))
+        Inventory.remove(item);
+        currentRoom.addItem(item);
     }
     
     /**
