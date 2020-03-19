d45c97776cf5a349b3172cfede02bc92d93c5753
pseudoprosciutto <55765602+pseudoprosciutto@users.noreply.github.com>
2020-03-19 15:22:31 -0400

    Update Message.java
    Message.java holds long strings for different items.
    itemDesciption (String item) method takes the item.getName() and returns it's description
    
    also added was printHelp() still needs adjustment

--- Message.java
+++ Message.java
@@ -9,6 +9,11 @@ public class Message
 {
     // instance variables - replace the example below with your own
     private String flashlight;
+    private String rock;
+    private String map;
+    private String key;
+    private String backpack;
+    
     /**
      * Constructor initializes all the messages to strings which will be 
      * called in the get method. specifically descriptions
@@ -16,9 +21,15 @@ public class Message
     public Message()
     {
         //ITEM STRINGS:
-        flashlight = "This flashlight is one of those heavy, powerful ones. Hopefully it has " 
-                   + "enough battery life to serve its function. It's definitely heavy enough to "
-                   + "incapacitate someone before grabbing me if I could get a good swing or throw";
+        flashlight  = "This flashlight is one of those heavy, powerful ones. Hopefully it has " 
+                    + "enough battery life to serve its function. It's definitely heavy enough to "
+                    + "incapacitate someone before grabbing me if I could get a good swing or throw.";
+        rock        = "not a very big rock, but something that I could maybe throw for distraction.";
+        map         = "A fire exits map you. It got a little torn ripping it from its frame."
+                    + "in the wall. It shows the only ways to get to the lower floors "
+                    + "are through the stairs or elevator.";
+        key         = "This key is metal and shiny. Hopefully unlocks an important door";
+        backpack    = "Wearing this backpack should allow me to carry a couple more items";
         
         
         
@@ -44,12 +55,19 @@ public class Message
      * @param item which is going to return as a string
      * @return the String value of each item sent in param
      */
-    public String itemDescription(String item)
+    public String itemDescription(Items item)
     {
-        switch(item){
+        switch(item.getName()){
             
             case "flashlight": return flashlight; //returns field String init'ed in constructor
             
+            case "rock": return rock; 
+            
+            case "map" : return map;
+            
+            case "key" : return key;
+            
+            case "backpack" : return backpack;
             
             
             
@@ -65,14 +83,27 @@ public class Message
     public void printWelcome()
     {
         System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
-        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("Escape from the Hotel");
+        System.out.println("======================");
+        System.out.println("As you arrive into your room that you just checked into, you place your back ");
         System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
         System.out.println();
         System.out.println(Game.player().getCurrentRoom().getLongDescription());
     }
     
-    
+    /**
+     * Print out some help information.
+     * Here we print some stupid, cryptic message and a list of the 
+     * command words.
+     */
+    public void printHelp() 
+    {
+        System.out.println("You are lost. You are alone. You wander");
+        System.out.println("around at the university.");
+        System.out.println();
+        System.out.println("Your command words are:");
+        //parser.showCommands();
+    }
     
     /**
      * An example of a method - replace this comment with your own
