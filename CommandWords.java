6985c3c9c7a786c3794972e95cd3d0a1c25aa9c7
Matt <pseudoprosciutto@yahoo.com>
2020-03-23 13:23:21 -0400

    Update CommandWords.java
    minor comment updates

--- CommandWords.java
+++ CommandWords.java
@@ -7,8 +7,8 @@ import java.util.HashMap;
  * This class holds an enumeration of all command words known to the game.
  * It is used to recognise commands as they are typed in.
  *
- * @author  Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author  Matthew Sheehan Marcelle Tamegnon
+ * @version 2020.3.22
  */
 
 public class CommandWords
@@ -23,6 +23,7 @@ public class CommandWords
     public CommandWords()
     {
         validCommands = new HashMap<>();
+        
         for(CommandWord command : CommandWord.values()) {
             if(command != CommandWord.UNKNOWN) {
                 validCommands.put(command.toString(), command);
